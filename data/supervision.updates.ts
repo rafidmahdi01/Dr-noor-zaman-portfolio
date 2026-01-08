0fa90ae90c961b4560b940e68a9ea186b5a14516
Dr. Noor Zaman <noorzaman.jhanjhi@taylors.edu.my>
2026-01-08 17:04:59 +0800

    Remove example supervision entry

--- data/supervision.updates.ts
+++ data/supervision.updates.ts
@@ -1,9 +1,1 @@
-export const supervisions: any[] = [
-  {
-    "studentName": "rafid",
-    "level": "masters",
-    "topic": "portolio",
-    "year": "2026",
-    "status": "completed"
-  }
-];
+export const supervisions: any[] = [];
