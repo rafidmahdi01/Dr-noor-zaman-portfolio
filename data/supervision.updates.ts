be11425ddd33ae7e1a1ae9851448567bb3565233
rafidmahdi01 <abdullahalmahdi.rafid@sd.taylors.edu.my>
2026-01-13 16:53:26 +0800

    Added supervision: rafid

--- data/supervision.updates.ts
+++ data/supervision.updates.ts
@@ -1,1 +1,10 @@
-export const supervisions: any[] = [];
+export const supervisions: any[] = [
+  {
+    "studentName": "rafid",
+    "level": "undergraduate",
+    "topic": "project",
+    "year": "2026",
+    "status": "ongoing",
+    "role": "main-supervisor"
+  }
+];
