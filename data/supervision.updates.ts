e95f35f3404002b80245c9aa6c15ac2e99af74a4
rafidmahdi01 <abdullahalmahdi.rafid@sd.taylors.edu.my>
2026-01-13 17:12:00 +0800

    Update supervision.updates.ts

--- data/supervision.updates.ts
+++ data/supervision.updates.ts
@@ -1,10 +1,1 @@
-export const supervisions: any[] = [
-  {
-    "studentName": "rafid",
-    "level": "undergraduate",
-    "topic": "project",
-    "year": "2026",
-    "status": "ongoing",
-    "role": "main-supervisor"
-  }
-];
+export const supervisions: any[] = [];
